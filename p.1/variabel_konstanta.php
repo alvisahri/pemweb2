a48bb9d979da08f73f50c0ddc850b0ebc2ac377b
alvisahri <114494103+alvisahri@users.noreply.github.com>
2023-02-24 20:17:41 +0700

    Add files via upload

---
+++ p.1/variabel_konstanta.php
@@ -0,0 +1,6 @@
+<?php
+const PRODI = "Teknik Instalasi"; 
+define("KELAS","TI06");
+
+echo "Prodi saya : " . PRODI . "<br>";
+echo "Kelas saya : " . KELAS;
---
+++ p.1/variabel_user.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Alvi Sahri";
+$kelas = "TI06";
+$ipk = 3.99;
+
+echo "Nama saya adalah" . $nama . "<br>";
+echo "Kelas $kelas <br>";
+echo "ipk saya adalah" .  $ipk;
